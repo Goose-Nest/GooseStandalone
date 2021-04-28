59767e3e297ca7f852ec7646e3f09ea0250becf2
Oj <oj@oojmed.com>
2021-04-28 20:20:00 +0100

    [Lib] renameFile: unsupport dirs (revert)

--- src/lib/renameFile.js
+++ src/lib/renameFile.js
@@ -3,11 +3,7 @@ import { copyFileSync, rmSync, lstatSync } from 'fs';
 import copyDirSync from './copyDirSync.js';
 
 export default (oldPath, newPath) => {
-  if (lstatSync(join(from, element)).isFile()) {
-    copyFileSync(oldPath, newPath);
-  } else {
-    copyDirSync(oldPath, newPath);
-  }
+  copyFileSync(oldPath, newPath);
 
   rmSync(oldPath, { recursive: true, force: true });
 };
